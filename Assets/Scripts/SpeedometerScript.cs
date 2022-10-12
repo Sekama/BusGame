9169f013c6a7b883acff25e5c61e5b398bd0af3c
Seth Marrison <sethers932@gmail.com>
2022-10-12 17:18:29 +0200

    added Speedometer sign

---
+++ Assets/Scripts/SpeedometerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeedometerScript : MonoBehaviour
+{
+    public GameObject playableCharacter;
+    //public Text _text;
+    public TMP_Text _textMeshPro;
+    public Player_Controller _playerController;
+    
+    void Start()
+    {
+        _playerController = playableCharacter.GetComponent<Player_Controller>();
+    }
+
+    
+    void Update()
+    {
+        _textMeshPro.SetText(_playerController._moveSpeed.ToString("N0")); 
+    }
+}
