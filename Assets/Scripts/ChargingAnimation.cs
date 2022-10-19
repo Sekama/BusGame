d55606d14bfd6c02d640385295f16cd9974f4d0e
Jorge Cobacho_PC <108235595+Jhcob@users.noreply.github.com>
2022-10-19 22:44:05 +0200

    Charging station prefab FX

---
+++ Assets/Scripts/ChargingAnimation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargingAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator particleCharge01, particleCharge02, particleCharge03, particleCharge04;  
+    [SerializeField] private Animator particleArea;  
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            particleCharge01.SetBool("isCharging", true);
+            particleCharge02.SetBool("isCharging", true);
+            particleCharge03.SetBool("isCharging", true);
+            particleCharge04.SetBool("isCharging", true);
+            particleArea.SetBool("isCharging", true);
+        }    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            particleCharge01.SetBool("isCharging", false);
+            particleCharge02.SetBool("isCharging", false);
+            particleCharge03.SetBool("isCharging", false);
+            particleCharge04.SetBool("isCharging", false);
+            particleArea.SetBool("isCharging", false);
+        }        
+    }
+}
